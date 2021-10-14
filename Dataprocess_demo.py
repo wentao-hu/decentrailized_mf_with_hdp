b77fb976833b27f9d886e9f76be6f9218e88efe3
stevenhuwentao <stevenhwt@163.com>
2021-10-14 23:39:06 +0800

    upate

--- Dataprocess_demo.py
+++ Dataprocess_demo.py
@@ -2,13 +2,5 @@ from Dataset_explicit import Dataset_explicit
 from evaluate import evaluate_model
 import numpy as np
 
-dataset = Dataset_explicit('/home/sufe/Desktop/neural_collaborative_filtering/Data/ml-1m')
-train_rating_matrix = dataset.trainMatrix
-test_ratings= dataset.testRatings
-
-print(type(test_ratings))
-print(test_ratings[0])
-
-print(type(train_rating_matrix))
-print(train_rating_matrix[0])
-
+import os
+print(os.getcwd())
